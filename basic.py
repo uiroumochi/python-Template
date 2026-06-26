64c60fec57b34cb2be95318b219c6599e92e8189
uiroumochi <takashou364@sgh-tsukuba.org>
2026-06-26 09:08:01 +0900

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,6 @@
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet
+import sys,bisect
+sys.setrecursionlimit(2000000)
+def L():return list(map(int,input().split()))
+def M():return map(int,input().split())
