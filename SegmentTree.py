aa61c3dcfeeb2c9f81e83d7ee146c88ba06a4d34
uiroumochi <takashou364@sgh-tsukuba.org>
2026-06-29 12:29:48 +0900

    Update SegmentTree.py

--- SegmentTree.py
+++ SegmentTree.py
@@ -1,6 +1,14 @@
 n = int(input())
 a = list(map(int,input().split()))
-INF = 0
+
+#単位元
+#INF = 0                                # ←ここ変える！       
+
+#処理内容(ex.最大最小 区間和)                  
+def syori(a,b):          
+  #return a+b                           # ←ここ変える！！
+
+#変えない
 N = 1
 while N < n:
   N<<=1
@@ -14,35 +22,35 @@ for i in range(n):
 for i in range(N-1,0,-1):
   left = i*2
   right = i*2+1
-  segtree[i] = segtree[left]+segtree[right]
+  segtree[i] = syori(segtree[left],segtree[right])
 
-#処理内容(ex.最大最小 区間和)
-def syori(array):
-  return sum(array)
+
   
 #一点更新
 def OneUpdate(index,value): # ← 0_indexed
   index = index + N         # ← 1_indexed
-  segtree[index] += value   #更新
+  #segtree[index] += value    #加算更新
+  #segtree[index] = value     #代入更新          ←ここも変える
   p = index
   while p > 1:
     p >>= 1
     left = p*2
     right = p*2 +1
-    segtree[p] = syori([segtree[left],segtree[right]])
+    segtree[p] = syori(segtree[left],segtree[right])
 
 #区間取得{x|l<=x<r}
 def GetSection(l,r): 
-  use = []
+  ansL = INF
+  ansR = INF
   l += N
   r += N-1
   while l<=r:
     if l&1 :
-      use.append(segtree[l])
+      ansL = syori(ansL,segtree[l])
       l+=1
     if not(r&1):
-      use.append(segtree[r])
+      ansR = syori(segtree[r],ansR)
       r-=1
     l>>=1
     r>>=1
-  return syori(use)
+  return syori(ansL,ansR)
