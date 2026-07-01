029a299540d9ced9ffde833930fc7d435de2c31c
uiroumochi <takashou364@sgh-tsukuba.org>
2026-07-01 12:40:05 +0900

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
-from collections import defaultdict,deque
-from sortedcontainers import SortedSet
+#from collections import defaultdict,deque
+#from sortedcontainers import SortedSet
 import sys,bisect
 sys.setrecursionlimit(2000000)
+input = lambda: sys.stdin.readline().rstrip()
 def L():return list(map(int,input().split()))
 def M():return map(int,input().split())
